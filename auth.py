844087d3498dfd017159a114a93fb9c677c47455
Shasmit <shasmitbasnet606@gmail.com>
2022-07-21 08:05:16 +0545

    Login and signup aunthenticated

--- auth.py
+++ auth.py
@@ -1,35 +1,44 @@
+
+import email
 from flask import Blueprint, flash, render_template, request, redirect, url_for
 from flask_login import login_user,current_user, logout_user, login_required
 from models import user as User
 from forms import RegistrationForm,LoginForm
+from werkzeug.security import generate_password_hash, check_password_hash
 from __init__ import db, bcrypt
 
 auth = Blueprint("auth", __name__)
-
 
 @auth.route("/register", methods=["GET", "POST"])
 def Register():
     form_signup = RegistrationForm()
     if current_user.is_authenticated:
-        return redirect(url_for("views.landingpage"))
+        return redirect(url_for("views.home"))
     if request.method == "POST":
         if form_signup.validate_on_submit():
-            hashed_password = bcrypt.generate_password_hash(
-                form_signup.password.data
-            ).decode("utf-8")
-            user = User(
-                username=form_signup.username.data,
-                email=form_signup.email.data,
-                password=hashed_password,
-            )
-            print(user)
-            db.session.add(user)
-            db.session.commit()
-            flash(
-                "Your account has been created! You are now able to log in",
-                "success",
-            )
-            return redirect(url_for("auth.Login"))
+            if User.query.filter_by(email=form_signup.email.data).first():
+                flash("Email already exists", "Email-Used")
+                return redirect(url_for("auth.Register"))
+            elif User.query.filter_by(username=form_signup.username.data).first():
+                flash("Username already exists", "Username-Used")
+                return redirect(url_for("auth.Register"))
+            else:
+                hashed_password = bcrypt.generate_password_hash(
+                    form_signup.password.data
+                ).decode("utf-8")
+                user = User(
+                    username=form_signup.username.data,
+                    email=form_signup.email.data,
+                    password=hashed_password,
+                )
+                print(user)
+                db.session.add(user)
+                db.session.commit()
+                flash(
+                    "Your account has been created! You are now able to log in",
+                    "success",
+                )
+                return redirect(url_for("auth.Login"))
     return render_template(
         "Signup/register.html",
         title="Signup",
@@ -42,7 +51,14 @@ def Login():
     form_login = LoginForm()
     if form_login.validate_on_submit():
         user = User.query.filter_by(email=form_login.email.data).first()
-        if user and bcrypt.check_password_hash(
+        if not user:
+            flash("No user found with that email", "Email-Not-Found")
+            return redirect(url_for("auth.Login"))
+        elif not bcrypt.check_password_hash(user.password, form_login.password.data):
+            flash("Password is incorrect", "Password-Incorrect")
+            return redirect(url_for("auth.Login"))        
+
+        elif user and bcrypt.check_password_hash(
             user.password, form_login.password.data
         ):
             login_user(user, remember=True)
@@ -50,7 +66,7 @@ def Login():
             return (
                 redirect(next_page)
                 if next_page
-                else redirect(url_for("views.landingpage"))
+                else redirect(url_for("views.home"))
             )
         else:
             flash("Login Unsuccessful. Please check email and password", "danger")
@@ -63,4 +79,4 @@ def Login():
 @auth.route("/Logout")
 def Logout():
     logout_user()
-    return redirect(url_for("views.home"))
+    return redirect(url_for("views.landingpage"))
--- views.py
+++ views.py
@@ -1,6 +1,21 @@
 from flask import Blueprint, render_template,redirect,request,url_for,flash
+from flask_login import login_user,current_user,logout_user,login_required
 
 views = Blueprint('views', __name__)
 @views.route('/')
 def landingpage():
     return render_template('LandingPage/landingpage.html')
+
+@views.route('/home')
+def home():
+    if not current_user.is_authenticated:
+        return redirect(url_for('auth.Login'))
+    return render_template('home/index.html')
+
+@views.route('/Products')
+def Products():
+    return render_template('Products/products.html')
+
+@views.route('/Profile')
+def Profile():
+    return render_template('Profile/profile.html')
